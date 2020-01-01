331642ed365a77eb46ab28ba954c3508e8e285a5
gyan <gyanender888@gmail.com>
2020-01-01 12:19:54 +0530

    changes in mqtt client

--- clientMqtt.py
+++ clientMqtt.py
@@ -6,29 +6,56 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from config import Config
 from models.rfCards import *
+import threading
 allLoc=LocationDetails.query.all()  
 allLoc=allLoc[0] if allLoc else {id:None}
 
 
-def read_rfid():   
-    ser = serial.Serial ("/dev/ttyUSB0")                           #Open named port 
-    ser.baudrate = 9600                                            #Set baud rate to 9600
-    data = ser.read(12)                                            #Read 12 characters from serial port to data
-    ser.close ()                                                   #Close port   
-    return {"data":data,"status":200} 
+def mqttProcess():
 
-def start():    
-    while(True):
-        data=read_rfid()
-        if (data.get("status",False)):
-            print ("Data insude ",data["data"],Config.CLIENT_topic.format(allLoc.id))            
-            publish.single(Config.CLIENT_topic.format(allLoc.id), payload=data["data"], qos=2, retain=False, hostname=Config.HOSTNAME,
-                       port=Config.CLIENT_port, client_id="", keepalive=60, will=None,tls = {'ca_certs': '/etc/ssl/certs/ca-certificates.crt'}, 
-                       protocol=mqtt.MQTTv31,auth={'username':Config.CLIENT_username, 'password':Config.CLIENT_password})
-            print ("Message sent successfuly")
+    def on_log(client, userdata, level, buf):
+        print ("This is the buffer {}".format(buf))
 
-        else:
-            print("data not received ")
-            
-print ("starting the process")
-start()
+
+    def on_publish(client, obj, mid):
+        print("This is the  mid In on Pub call back: " + str(mid))
+
+    def on_message(client, userdata, msg):
+        print ("######################")
+        print ("Topic: ", msg.topic + '  Message: ' + str(msg.payload))
+        print ("######################")
+
+
+    def read_rfid():   
+        ser = serial.Serial ("/dev/ttyUSB0")                           #Open named port 
+        ser.baudrate = 9600                                            #Set baud rate to 9600
+        data = ser.read(12)                                            #Read 12 characters from serial port to data
+        ser.close()                                                   #Close port               
+        return {"data":data,"status":200} 
+
+    def start():          
+        while(1):      
+            data=read_rfid()
+            if (data.get("status",False)):
+                print ("Data insude ",data["data"],Config.CLIENT_topic.format(allLoc.id))            
+                client.publish(Config.CLIENT_topic.format(allLoc.id),data["data"], qos=2, retain=False)                        
+            else:
+                print("data not received ")
+        
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        print ("Making Default Values____________________________________________________")
+        client.subscribe("/home/rfid/#")
+        threading.Thread(target=start).start()
+        
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.on_log = on_log
+    client.tls_set('/etc/ssl/certs/ca-certificates.crt',tls_version=2)
+    client.username_pw_set(Config.CLIENT_username,Config.CLIENT_password)
+    client.connect(Config.HOSTNAME, Config.CLIENT_port, 60)
+    client.loop_forever()
+
+mqttProcess()
